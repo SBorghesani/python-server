585c15fe5fd969779d8892463e5361321a6633dd
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-10-15 15:58:28 -0500

    mid chapter 15

--- kennel.sql
+++ kennel.sql
@@ -33,7 +33,19 @@ CREATE TABLE `Employee` (
 
 );
 
-SELECT * FROM animal
 
-
-
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address,
+	c.name customer_name
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
+JOIN Customer c 
+	ON c.id = a.customer_id
